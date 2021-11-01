3e0b769e5a709f7b30519bbb0e29df8e967d1e55
Faisal <falfariheedi@gmail.com>
2021-11-01 17:14:28 +0300

    completed task class

--- app/src/main/java/com/example/task_timer/db/Task.kt
+++ app/src/main/java/com/example/task_timer/db/Task.kt
@@ -1,4 +1,19 @@
 package com.example.task_timer.db
 
-class Task {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName="Task")
+class Task(
+    @ColumnInfo(name="Name")
+    var name: String,
+    @ColumnInfo(name="Description")
+    var desc:String,
+    @ColumnInfo(name="Time_spent")
+    var Time_spent:Int,
+    @PrimaryKey(autoGenerate=true)
+    @ColumnInfo(name="ID")
+    var id:Int
+)
