95526dd42435a4011f8d304ed3bd0ddda72dfaae
Faisal <falfariheedi@gmail.com>
2021-11-01 17:19:25 +0300

    TaskDao completed

--- app/src/main/java/com/example/task_timer/db/TaskDao.kt
+++ app/src/main/java/com/example/task_timer/db/TaskDao.kt
@@ -1,4 +1,17 @@
 package com.example.task_timer.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
 interface TaskDao {
+    @Query("SELECT * from Task")
+    fun getall(): LiveData<List<Task>>
+
+    @Insert(onConflict= OnConflictStrategy.REPLACE)
+    fun addeditTask(task:Task)
+
+    @Delete
+    fun deleteTask(task:Task)
 }
