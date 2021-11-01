a0c0c6a74d1320bdb73cf04445933286e70accf9
Faisal <falfariheedi@gmail.com>
2021-11-01 18:45:25 +0300

    repo and viewmodel are done and ready to be used

--- app/src/main/java/com/example/task_timer/ViewModel.kt
+++ app/src/main/java/com/example/task_timer/ViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.task_timer
 
-class ViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.task_timer.db.Task
+
+class ViewModel(application: Application) : AndroidViewModel(application) {
+    var rep=repo(application)
+    private var list=rep.getAll()
+
+
+    fun addedit(t: Task){
+        rep.addedit(t)
+    }
+    fun delete(note: Task){
+        rep.delete(note)
+    }
+    fun getAll(): LiveData<List<Task>> {
+        return list
+    }
 }
--- app/src/main/java/com/example/task_timer/repo.kt
+++ app/src/main/java/com/example/task_timer/repo.kt
@@ -1,9 +1,43 @@
 package com.example.task_timer
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+import com.example.task_timer.db.Task
+import com.example.task_timer.db.TaskDB
+import com.example.task_timer.db.TaskDao
 
-class repo(application: Application) : AndroidViewModel(application) {
-    var rep=repo(application)
-//    private var list=rep.getAll()
+class repo{
+    var db:TaskDao
+    var list:LiveData<List<Task>>
+
+    constructor(cont:Application){
+        db=TaskDB.getInstance(cont).TaskDao()
+        list=db.getall()
+    }
+
+    fun addedit(t: Task) {
+        insTask(db).execute(t)
+    }
+
+    fun delete(t: Task) {
+        delTask(db).execute(t)
+    }
+
+    fun getAll(): LiveData<List<Task>> {
+        return list
+    }
+
+    class insTask(var db: TaskDao) : AsyncTask<Task, Void, String>(){
+        override fun doInBackground(vararg p0: Task?): String {
+            db.addeditTask(p0[0]!!)
+            return ""
+        }
+    }
+    class delTask(var db: TaskDao) : AsyncTask<Task, Void, String>(){
+        override fun doInBackground(vararg p0: Task?): String {
+            db.deleteTask(p0[0]!!)
+            return ""
+        }
+    }
 }
