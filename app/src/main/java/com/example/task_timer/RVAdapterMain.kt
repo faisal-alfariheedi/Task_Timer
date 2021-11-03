b76e26e296a08ba6459e821fa49721a9d6164ead
Faisal <falfariheedi@gmail.com>
2021-11-03 16:01:40 +0300

    modified recycle view ui update

--- app/src/main/java/com/example/task_timer/RVAdapterMain.kt
+++ app/src/main/java/com/example/task_timer/RVAdapterMain.kt
@@ -20,12 +20,12 @@ import android.os.CountDownTimer
 
 
 
-class RVAdapter(private val rv: ArrayList<Task>, val cont: Context,var sel:Int): RecyclerView.Adapter<RVAdapter.ItemViewHolder>()  {
+class RVAdapterMain(private val rv: ArrayList<Task>, val cont: Context, var sel:Int): RecyclerView.Adapter<RVAdapterMain.ItemViewHolder>()  {
 
     class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         return ItemViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.rvlist,parent,false )
+            LayoutInflater.from(parent.context).inflate(R.layout.rvlisttime,parent,false )
         )
     }
 
